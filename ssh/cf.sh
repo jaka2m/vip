49fa49890b6dbb9a1a3ab48f95cd35ee1955ddcb
WAYAN MURDIONI <154336943+jaka2m@users.noreply.github.com>
2025-07-16 11:59:00 +0800

    Update cf.sh

--- ssh/cf.sh
+++ ssh/cf.sh
@@ -38,7 +38,7 @@ RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_r
      -H "Content-Type: application/json" \
      --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
 echo "Host : $SUB_DOMAIN"
-echo "IP=" >> /var/lib/kyt/ipvps.conf
+echo "IP=" >> /var/lib/geovpn/ipvps.conf
 echo $SUB_DOMAIN > /etc/xray/domain
 echo $SUB_DOMAIN > /root/domain
 rm -f /root/cf.sh
