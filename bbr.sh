8ac7716e249ca2bf873a37b3a5bbba2e6ae3493c
WAYAN MURDIONI <154336943+jaka2m@users.noreply.github.com>
2025-07-16 17:14:02 +0800

    Update bbr.sh

--- bbr.sh
+++ bbr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Edition : Stable Edition V3.0
-# Auther  : ALVI TUNNEL
+# Auther  : GEO PROJECT
 # (C) Copyright 2023
 # =========================================
 
@@ -18,7 +18,7 @@ clear
 #	 menu
 #else
 
-echo -e "Installing TCP BBR Mod By Kyt & Ridwan"
+echo -e "Installing TCP BBR Mod By GEO PROJECT"
 echo -e "Please Wait BBR Installation Will Starting . . ."
 sleep 5
 clear
